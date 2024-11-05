14978b00c2a54755e5440d5ee84b3bcd14e4f2ce
Svetlana Smirnova <smirnovasv2030@yandex.ru>
2024-11-05 16:57:30 +0300

    первый коммит

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,25 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardContainer = document.querySelector('.places__list');
+
+initialCards.forEach(function (element) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardDescription = cardElement.querySelector('.card__description');
+
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelectorAll('.card__description').textContent = element.name;
+  cardElement.querySelector('.card__delete-button').textContent = element.button;
+  cardDescription.querySelector('.card__title').textContent = element.name;
+
+  cardContainer.append(cardElement);
+
+  const cardDelete = cardElement.querySelector('.card__delete-button');
+
+  cardDelete.addEventListener('click', function () {
+  cardElement.remove();
+})
+});
+
