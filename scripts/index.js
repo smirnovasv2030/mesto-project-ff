3b672e05bb574a12cd3dbb795ca6a35f6d2b20f9
Svetlana Smirnova <smirnovasv2030@yandex.ru>
2024-11-06 23:00:17 +0300

    второй коммит

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,40 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
+const cardTemplate = document.querySelector('#card-template').content;
+const cardsContainer = document.querySelector('.places__list');
+const cardElement = cardTemplate.querySelector('.card');
 
 // @todo: Функция создания карточки
+//устанавливаем данные карточки и обработчик клика по корзинке удаления
+function createCard(data, onDelete) {
+
+  const newCard = cardElement.cloneNode(true);
+
+  const newCardImage = newCard.querySelector('.card__image');
+  newCardImage.src = data.link;
+  newCardImage.alt = data.name;
+
+  newCard.querySelector('.card__title').textContent = data.name;
+
+  const cardDeleteButton = newCard.querySelector('.card__delete-button');
+  cardDeleteButton.addEventListener('click', function () {
+      onDelete(newCard);
+  });
+
+return newCard;
+};
 
 // @todo: Функция удаления карточки
+function handleDeleteCard(cardElement) {
+      cardElement.remove();
+};
 
-// @todo: Вывести карточки на страницу
+// @todo: функция вставки карточки на страницу
+function renderCard(card, cardsContainer) {
+  cardsContainer.append(card);//функционал вставки элемента карточки на страницу
+};
 
-const cardContainer = document.querySelector('.places__list');
 
-initialCards.forEach(function (element) {
-  const cardTemplate = document.querySelector('#card-template').content;
-  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  const cardDescription = cardElement.querySelector('.card__description');
-
-  cardElement.querySelector('.card__image').src = element.link;
-  cardElement.querySelectorAll('.card__description').textContent = element.name;
-  cardElement.querySelector('.card__delete-button').textContent = element.button;
-  cardDescription.querySelector('.card__title').textContent = element.name;
-
-  cardContainer.append(cardElement);
-
-  const cardDelete = cardElement.querySelector('.card__delete-button');
-
-  cardDelete.addEventListener('click', function () {
-  cardElement.remove();
-})
+// @todo: Вывести карточки на страницу, используем цикл forEach
+initialCards.forEach(initialCard => {
+  const card = createCard(initialCard, handleDeleteCard);
+  renderCard(card, cardsContainer);
 });
-
